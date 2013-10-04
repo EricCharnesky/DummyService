a4f3a0cbf45a7b3308ef28f66b55bbee77de33d3
Eric Charnesky <Eric@careevolution.com>
2013-10-04 04:52:40 -0400

    Dummy Service v1

---
+++ DummyService/DummyService.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace DummyService
+{
+	public partial class DummyService : ServiceBase
+	{
+		public DummyService()
+		{
+			InitializeComponent();
+			if ( !EventLog.SourceExists( "DummyService" ) )
+			{
+				EventLog.CreateEventSource( "DummyService", "DummyServiceLog" );
+			}
+			eventLog1.Source = "DummyService";
+			eventLog1.Log = "DummyServiceLog";
+			_pollingReader = new PollingReader( eventLog1 );
+		}
+
+		protected override void OnStart( string[] args )
+		{
+			eventLog1.WriteEntry( "Service Starting" );
+			_pollingReader.Start();
+
+		}
+
+		protected override void OnStop()
+		{
+			eventLog1.WriteEntry( "Service Stopping" );
+			_pollingReader.Stop();
+		}
+
+		private PollingReader _pollingReader;
+	}
+}
---
+++ DummyService/PollingReader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace DummyService
+{
+	class PollingReader
+	{
+		public PollingReader( EventLog eventLog )
+		{
+			_eventLog = eventLog;
+		}
+
+		public bool IsRunning { get; private set; }
+
+		public void Start()
+		{
+			if ( IsRunning ) throw new InvalidOperationException( "Already running" );
+
+			_pollingReader = new Thread( new ThreadStart( Poll ) );
+			_pollingReader.Start();
+		}
+
+		public void Stop()
+		{
+			if ( !IsRunning ) throw new InvalidOperationException( "Not running" );
+
+			_pollingReader.Abort();
+			_pollingReader.Join();
+
+			IsRunning = false;
+		}
+
+		private void Poll()
+		{
+			IsRunning = true;
+
+			try
+			{
+				while ( true )
+				{
+					String logFile = @"E:\Program Files\DummyService\DummyFile.txt";
+					Thread.Sleep( 10000 );
+					TextReader reader = new StreamReader( logFile );
+					_eventLog.WriteEntry( reader.ReadToEnd() );
+					reader.Close();
+				}
+			}
+			catch ( IOException e )
+			{
+				_eventLog.WriteEntry( e.ToString(), EventLogEntryType.Error );
+				throw;
+			}
+			catch ( ThreadAbortException )
+			{
+				// Do nothing -- Stop was called
+			}
+		}
+
+		private EventLog _eventLog;
+		private Thread _pollingReader;
+	}
+}
